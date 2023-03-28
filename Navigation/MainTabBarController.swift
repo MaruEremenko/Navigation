eb8e885eebedd105ab2364e7ce32ae24a0fd42d6
Marina Eremenko <kobyzeva13@gmail.com>
2023-03-28 19:39:59 +0300

    Fixed remarks

--- Navigation/MainTabBarController.swift
+++ Navigation/MainTabBarController.swift
@@ -1,23 +1,28 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
+    
+    private let profile = ProfileViewController()
+    private let newsFeed = FeedViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
         setupControllers()
     }
 
     func setupControllers() {
-        let feedVC = createController(viewController: FeedViewController(), itemName: "News", ItemImage: "newspaper")
-        let profileVC = createController(viewController: ProfileViewController(), itemName: "Profile", ItemImage: "person.circle")
-        viewControllers = [feedVC, profileVC]
-    }
-
-    func createController(viewController: UIViewController, itemName: String, ItemImage: String) -> UINavigationController {
-        let item = UITabBarItem(title: itemName, image: UIImage(systemName: ItemImage)?.withAlignmentRectInsets(.init(top: 10, left: 0, bottom: 0, right: 0))  ,tag: 0)
-        item.titlePositionAdjustment = .init(horizontal: 0, vertical: 10)
-        let navigationController = UINavigationController(rootViewController: viewController)
-        navigationController.tabBarItem = item
-        return navigationController
+        let navigationProfile = UINavigationController(rootViewController: profile)
+        let navigationNewsFeed = UINavigationController(rootViewController: newsFeed)
+        
+        profile.tabBarItem.title = "Profile"
+        profile.tabBarItem.image = UIImage(systemName: "folder")
+    
+        newsFeed.tabBarItem.title = "Feed"
+        newsFeed.tabBarItem.image = UIImage(systemName: "note")
+        
+    
+        
+        viewControllers = [navigationNewsFeed, navigationProfile]
     }
 }
